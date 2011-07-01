19e0d681eb319bfecbeb4478dc42d412a65e8221
Tom Adams <tom@holizz.com>
2011-07-01 09:51:43 -0400

    Usage

--- fdl.sh
+++ fdl.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+[ X$1 = X ] && echo 'Usage: fdl.sh USERNAME' && exit 1
+
 USER=$1
 
 # End case: 302 Found
@@ -11,10 +13,9 @@ PICURL=http://www.flickr.com/photos/%s/%s/sizes/o/
 N=1
 while true; do
   U=`printf $URL $USER $N`
-  echo $U
-
   STATUS=`curl -I $U 2>/dev/null | head -n1 | cut -d' ' -f2`
   [ $STATUS = 302 ] && break
+  echo $U
 
   curl $U 2>/dev/null | grep '<a name="photo' | cut -d'"' -f2 | cut -b 6-
 
